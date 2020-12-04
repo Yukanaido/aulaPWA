a2f96ce0e83a51af345818184567223874587a78
Yukanaido <fabricioakiraa@gmail.com>
2020-12-04 11:15:07 -0300

    Commit inicial

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,3 +1,24 @@
+var cacheName = 'fabricio_cache-v1'
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
 // This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
